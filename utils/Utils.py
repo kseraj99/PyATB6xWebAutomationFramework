28f144e041306d30cf46fee73c48206c348728ea
kseraj99 <serajkhan554u@gmail.com>
2026-01-31 16:01:14 +0530

    Apply gitignore and clean tracked/untracked files

---
+++ utils/Utils.py
@@ -0,0 +1,6 @@
+import allure
+
+def take_screen_shot(driver, name):
+    allure.attach(driver.get_screenshot_as_png(),
+                  name = name,
+                  attachment_type=allure.attachment_type.PNG)
